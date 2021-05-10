db3b037495a7ac903187106d6fd0f467064e7d11
happy-boksil2 <happyBoksil2@gmail.com>
2021-05-10 23:48:54 +0900

    first commit

---
+++ front/pages/profile.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      profile Page
+  </div>
+</template>
+
+<script>
+export default {
+    layout: 'admin',
+    data() {
+        return {
+            name: "profile",
+        }
+    },
+    head: {
+        title: "프로필"
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ front/pages/signup.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    signup Page
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: 'signup page'
+        }
+    },
+    head: {
+        title: '회원가입페이지'
+    }
+}
+</script>
+
+<style>
+
+</style>
